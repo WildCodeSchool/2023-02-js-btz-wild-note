c55f466824fe94c9eb9d3408bfa99c582c667ce2
ELOISE BROCHIER <eloise.brochier@gmail.com>
2023-04-18 12:23:22 +0200

    Commencement du css de la page login + logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Library from './pages/Library';
+import Login from './pages/Login';
+
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path='/login' element={<Login />} />
         <Route path='/library' element={<Library />} />
+
       </Routes>
     </Router>
   );

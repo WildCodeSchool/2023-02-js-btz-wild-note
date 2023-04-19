5205ecf0b43781a032d4c44af07a0e0495f67647
Axelastach <axelpicaut@orange.fr>
2023-04-19 11:25:34 +0200

    searchpage and cardbygender done

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Library from './pages/Library';
-<<<<<<< HEAD:src/App.js
 import Login from './pages/Login';
 
-=======
-import SearchBar from './components/SearchBar/SearchBar';
->>>>>>> dev:src/App.jsx
+
+import SearchPages from './pages/SearchPages';
 
 function App() {
   return (
     <Router>
-      <SearchBar/>
+      
       <Routes>
         <Route path='/login' element={<Login />} />
         <Route path='/library' element={<Library />} />
-
+        <Route path='/search' element={<SearchPages/>}/>
       </Routes>
     </Router>
   );

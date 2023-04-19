e9f5918ebf9e4bea9e506e717e88c4302eb66986
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-04-19 11:22:36 +0200

    homepage design done

---
+++ src/components/YourArtists/YourArtists.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './YourArtists.css';
+
+const YourArtists = () => {
+  return (
+    <div className='artists-container'>
+        <h2>YOUR ARTISTS</h2>
+    </div>
+  )
+}
+
+export default YourArtists

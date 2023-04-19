5c8b4e2594b23d2e1a6ec46afcaa9214483466c9
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-04-19 09:58:59 +0200

    fix css

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import SearchBar from '../components/SearchBar/SearchBar';
+import Navbar from '../components/navbar/Navbar';
+import './Home.css';
+
+
+const Home = () => {
+  return (
+    <div className='Home'>
+
+    </div>
+  )
+}
+
+export default Home
